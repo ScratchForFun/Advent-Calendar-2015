866c51e116780919ebbc5cb1d645c3fef8ebb9bb
ScratchForFun <magnusvaagenmorud@gmail.com>
2015-12-01 21:20:44 +0100

    Episode 1
    Added the Frame and Main classes

---
+++ src/net/scratchforfun/Frame.java
@@ -0,0 +1,30 @@
+package net.scratchforfun;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Magnus on 10/15/2015.
+ */
+public class Frame {
+
+    public static String TITLE = "Advent Calendar Game";
+    public static int WIDTH = 640, HEIGHT = 480;
+
+    private JFrame frame;
+
+    public Frame() {
+        frame = new JFrame(TITLE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Dimension dim = new Dimension(WIDTH, HEIGHT);
+        frame.getContentPane().setPreferredSize(dim);
+
+        frame.setResizable(false);
+        frame.pack();
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+}
