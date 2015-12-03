2d3c32eec870d9ba3bb54756ea19ead22086fbb9
ScratchForFun <magnusvaagenmorud@gmail.com>
2015-12-04 00:22:50 +0100

    Episode 3
    Finished the Game loop

--- src/net/scratchforfun/Screen.java
+++ src/net/scratchforfun/Screen.java
@@ -9,6 +9,10 @@ public class Screen extends Canvas implements Runnable {
 
     public static final int TARGET_UPS = 60;
 
+    private boolean isRunning = true;
+    private boolean isDebugging = true;
+    private boolean throttleFps = false;
+
     private Thread thread;
 
     public Screen() {
@@ -21,8 +25,8 @@ public class Screen extends Canvas implements Runnable {
 
     private void preRender() {}
     private void render() {}
-    private void tick() {}
     private void load() {}
+    private void tick() {}
 
     @Override
     public void run() {
@@ -31,6 +35,52 @@ public class Screen extends Canvas implements Runnable {
         int frames = 0, ticks = 0;
         double lastSecondMs = System.currentTimeMillis();
 
+        double then = System.nanoTime();
         double periodNs = 1000000000D / TARGET_UPS;
+        double unprocessed = 0;
+
+        while(isRunning) {
+            boolean canRender = false;
+            double now = System.nanoTime();
+            unprocessed += (now - then) / periodNs;
+            then = now;
+
+            while(unprocessed >= 1) {
+                ticks++;
+                tick();
+                canRender = true;
+                --unprocessed;
+            }
+
+            if(!throttleFps || canRender) {
+                frames++;
+                preRender();
+            }
+
+            if(System.currentTimeMillis() - 1000 > lastSecondMs) {
+                if(isDebugging)
+                    System.out.println("Frames: " + frames + ", Ticks: " + ticks);
+                frames = 0; ticks = 0;
+                lastSecondMs += 1000;
+            }
+
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) { }
+        }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
